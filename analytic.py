32638c1b436cb872b4fb6818cfeb5f85fc45815c
Max Cohn <mxc1805@rit.edu>
2019-02-16 15:53:53 -0500

    Made analytic branch and Analytic base class

---
+++ analytic.py
@@ -0,0 +1,17 @@
+# analytic.py
+#
+# Parent class of all analytics
+
+# Possible analytics:
+
+# Number of songs
+# Number of albums
+# Most common word
+# Size of vocab
+# Overall positivity/negativity
+# Release history
+
+class Analytic:
+
+    pass
+
