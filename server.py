02cc97471457a53f7cedebcd3c7b45c435a7bca4
Max Cohn <mxc1805@rit.edu>
2019-02-17 00:17:52 -0500

    got post request from index.html to server.py working

--- server.py
+++ server.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+import logging
+from logging.handlers import RotatingFileHandler
+import json
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("%s.html" % "index")
 
+@app.route('/analyze', methods=['POST'])
+def analyze():
+
+    app.logger.warning(request.get_json())
+
+    #TODO:
+    # request recieved, now we have to get artist ID
+    # if artist id is in database, return stats
+    # else, run analytics
+
+    return "gottem"
 
 if __name__ == '__main__':
-    app.run()
+    
+    app.run(debug=True)
