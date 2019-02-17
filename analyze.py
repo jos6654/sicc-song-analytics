778f25c3286c5772d1b6c9a47171e8b782a5f6f3
rmw1075 <rmw1075@g.rit.edu>
2019-02-16 21:55:00 -0500

    commit database VERSION 02/16/2019 21:31

---
+++ analyze.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("%s.html" % "index")
+
+
+if __name__ == '__main__':
+    app.run()
+
