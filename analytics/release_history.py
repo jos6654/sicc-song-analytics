f09f3ecbe4d8b46fcd50b4f1ffe1513f0d36d675
jos6654 <jos6654@rit.edu>
2019-02-16 21:32:07 -0500

    Added some documentation and changed up the release history class a little

--- analytics/release_history.py
+++ analytics/release_history.py
@@ -7,9 +7,18 @@ import re
 class ReleaseHistory(Analytic):
 
     def analyze(self):
+        """
+        This analytic subclass takes the list of song urls and iterates through them
+        scraping the release date of each song from the web page and returning it as an
+        integer.
+        :return: The
+        """
+        date_list = []
         for url in self.url_list:
             page = requests.get(url)
             html = BeautifulSoup(page.text, 'html.parser')
             date = html.select('.metadata_unit .metadata_unit-info--text_only')[0]
             result = re.match(r"\d{4}", date)[0]
+            date_list.append(result)
             print(result)
+        return date_list
